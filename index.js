9eefe491a4f9da74d05da3a07af62a920df35b16
Nils Knappmeier <n.knappmeier@i-views.de>
2016-01-10 00:43:04 +0100

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * deep-aplus <https://github.com/nknapp/deep-aplus>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function deepAplus () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * deep-aplus <https://github.com/nknapp/deep-aplus>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var deepAplus = require('../')
+
+describe('deep-aplus:', function () {
+  // body
+})
